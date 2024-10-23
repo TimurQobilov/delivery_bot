ff7256d422c035d3943b531969f8e901eb4db24e
Developer <qobilovtemur.dev@gmail.com>
2024-10-23 14:05:39 +0500

    добавил обращения по имени

--- bot.py
+++ bot.py
@@ -1,39 +1,34 @@
-from urllib.response import addbase
-
 import telebot
 import buttons as bt
 from geopy import Photon
 
 
 geolocator = Photon(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
-bot = telebot.TeleBot(token="8126152087:AAF3DN1hzlFBdpu6FNCsJPxAEGGSIRSijVY")
+bot = telebot.TeleBot(token="")
 
 @bot.message_handler(commands=["start"])
 def start(message):
     user_id = message.from_user.id
     bot.send_message(user_id, "Добро пожаловать в бот доставки!")
     bot.send_message(user_id, "Введите своё имя для регистрации")
-    print(message.text)
     bot.register_next_step_handler(message, get_name)
 
 def get_name(message):
     user_id = message.from_user.id
-    name = message.text
-    print(message.text)
-    bot.send_message(user_id, "Теперь поделитесь своим номером",
-                     reply_markup=bt.phone_button())
+    name = message.from_user.first_name
+    bot.send_message(user_id, f"Привет {name}!")
+    bot.send_message(user_id, f"Теперь поделитесь своим номером, {name}", reply_markup=bt.phone_button())
     bot.register_next_step_handler(message, get_phone_number, name)
 
 def get_phone_number(message, name):
+    name = message.from_user.first_name
     user_id = message.from_user.id
     if message.contact:
         phone_number = message.contact.phone_number
-        print(phone_number)
-        bot.send_message(user_id, "Отправьте свою локацию", reply_markup=bt.location_button())
-        bot.register_next_step_handler(message, get_location, name, phone_number)
     else:
-        bot.send_message(user_id, "Отправьте свой номер через кнопку в меню")
-        bot.register_next_step_handler(message, get_phone_number, name)
+        phone_number = message.text  # Если номер введен вручную
+    bot.send_message(user_id, f"Отправьте свою локацию {name}", reply_markup=bt.location_button())
+    bot.register_next_step_handler(message, get_location, name, phone_number)
 
 def get_location(message, name, phone_number):
     user_id = message.from_user.id
@@ -43,8 +38,9 @@ def get_location(message, name, phone_number):
         address = geolocator.reverse((latitude, longitude)).address
         print(name, phone_number, address)
         bot.send_message(user_id, "Вы успешно зарегистрировались!")
-        bot.send_message(user_id, "Главное меню: ")
-
+        bot.send_message(user_id, "Главное меню:")
+    else:
+        bot.send_message(user_id, "Пожалуйста, отправьте свою локацию через кнопку")
+        bot.register_next_step_handler(message, get_location, name, phone_number)
 
 bot.infinity_polling()
-
